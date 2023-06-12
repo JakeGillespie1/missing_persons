924e382555a253d66fa180209c74fc4bfc9085d9
JakeGillespie1 <102985541+JakeGillespie1@users.noreply.github.com>
2023-06-12 10:29:54 -0600

    Update views.py
    add context for dynamic use

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def indexPageView(request):
+def indexPageView(request, context):
+    context = {}
     return render(request, "index.html")
