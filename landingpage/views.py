4fa6b178af39f3ec66b3f4898a99eff345f214f7
JakeGillespie1 <102985541+JakeGillespie1@users.noreply.github.com>
2023-06-12 11:56:28 -0600

    Update views.py

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def indexPageView(request, context):
-    context = {}
+def indexPageView(request):
     return render(request, "index.html")
