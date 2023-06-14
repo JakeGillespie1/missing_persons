bdb1be4d3094177cb89b624ff13e8a20251098ce
JakeGillespie1 <102985541+JakeGillespie1@users.noreply.github.com>
2023-06-14 13:48:34 -0600

    Update settings.py

--- humantraffic/settings.py
+++ humantraffic/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'missing_persons',
     'USER': 'postgres',
-    'PASSWORD': 'password01',
+    'PASSWORD': '',
     'HOST': 'localhost'
     }
 } 
