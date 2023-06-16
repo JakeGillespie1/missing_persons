e9d0544b9196b6bd78b85aa83c60babf001af49a
JakeGillespie1 <102985541+JakeGillespie1@users.noreply.github.com>
2023-06-15 20:42:44 -0600

    Update views.py
    Added context and connect to database

--- infotable/views.py
+++ infotable/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .models import Person
 
 # Create your views here.
 def infoTableView(request):
-    return render(request, "infotable/table.html")
+    data = Person.objects.all()
 
+    context = {
+        "missing_persons" : data
+    }
 
+    return render(request, "infotable/table.html", context)
+
+def individualPersonView(request, personID):
+    data = Person.objects.filter(
+        id = personID
+    ).get()
+
+    context = {
+        "person" : data
+    }
+
+    return render(request, "infotable/person.html", context)
