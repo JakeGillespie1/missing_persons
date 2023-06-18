3c8a96ee9ad67d7b1cbbf8b1c95bdb05f6fde657
ztullis <131419446+ztullis@users.noreply.github.com>
2023-06-17 19:00:20 -0600

    Added individualPersonView to the 2nd line, added a comma on line 6

--- infotable/urls.py
+++ infotable/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import infoTableView
+from .views import infoTableView, individualPersonView
 
 
 urlpatterns = [
-    path('', infoTableView, name="infoTableView")
+    path('', infoTableView, name="infoTableView"),
     path('person/<str:personID>', individualPersonView, name="personView")
 ]
